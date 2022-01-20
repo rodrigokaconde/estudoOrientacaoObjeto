127f994535d6d7c6981d6e44fa5ec0baff9e5686
rodrigokaconde <rodrigokaconde@yahoo.com.br>
2022-01-20 13:56:19 -0300

    created base project

--- src/main/java/com/rfoliveira/bancoApi/BancoApiApplication.java
+++ src/main/java/com/rfoliveira/bancoApi/BancoApiApplication.java
@@ -7,7 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BancoApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BancoApiApplication.class, args);
+
+		Cliente rodrigo = new Cliente();
+		rodrigo.setNome("Rodrigo");
+
+		Conta cc = new ContaCorrente(rodrigo);
+		Conta poupanca = new ContaPoupanca(rodrigo);
+
+		cc.depositar(100);
+
+		cc.transferir(50, poupanca);
+
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+
 	}
 
 }
