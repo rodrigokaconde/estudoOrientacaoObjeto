fdaf4f03a1c73d1fe08d9783533eb40f32c3a13f
rodrigokaconde <rodrigokaconde@yahoo.com.br>
2022-01-20 13:08:29 -0300

    commit init

---
+++ src/test/java/com/rfoliveira/bancoApi/BancoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfoliveira.bancoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
